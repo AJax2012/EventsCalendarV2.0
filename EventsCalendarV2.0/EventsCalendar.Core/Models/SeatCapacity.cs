f46d09a7f0c3b38e37b29b2a809ca682364d3674
Adam Gardner <adga2012@gmail.com>
2019-03-03 19:02:27 -0700

    Change SeatCapacity & Venue Views

--- EventsCalendarV2.0/EventsCalendar.Core/Models/SeatCapacity.cs
+++ EventsCalendarV2.0/EventsCalendar.Core/Models/SeatCapacity.cs
@@ -11,5 +11,6 @@ namespace EventsCalendar.Core.Models
         public int Budget { get; set; }
         public int Moderate { get; set; }
         public int Premier { get; set; }
+        public int Total { get; set; }
     }
 }
